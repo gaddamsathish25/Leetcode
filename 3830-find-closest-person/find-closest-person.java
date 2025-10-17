55f6f62c87e6d5bcb74ba142caf52632149e5f53
Gaddam Sathish <gaddam.sathish2510@gmail.com>
2025-10-18 00:04:45 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (30.48%) - LeetSync

---
+++ 3830-find-closest-person/find-closest-person.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        int a=Math.abs(z-x);
+        int b=Math.abs(z-y);
+
+        if(a<b){
+         return 1;   
+        } 
+        else if(b<a){
+         return 2;   
+        } 
+        else if(a==b){
+         return 0;   
+        }
+        else{
+            return-1;
+        }
+
+    }
+    
+}
