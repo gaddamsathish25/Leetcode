550775cccb00e4d97c417a25e0db8043a3161671
Gaddam Sathish <gaddam.sathish2510@gmail.com>
2026-03-06 11:31:50 +0530

    Time: 1 ms (99.87%) | Memory: 45.3 MB (53.78%) - LeetSync

---
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findLucky(int[] arr) {
+        int freq[]=new int [501];
+
+        int n=arr.length;
+
+        for(int i=0;i<n;i++){
+            freq[arr[i]]++;
+        }
+        
+        for(int i=500;i>=1;i--){
+
+        
+            if(freq[i]==i){
+                
+                return i;
+                
+            }
+            
+        }
+        return -1;
+    }
+}
