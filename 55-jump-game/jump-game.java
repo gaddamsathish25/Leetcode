f1196a58e09c3b685b074f9c659cbc766ad558e7
Gaddam Sathish <gaddam.sathish2510@gmail.com>
2026-05-03 00:45:32 +0530

    Time: 2 ms (86.97%) | Memory: 47.8 MB (61.41%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -2,17 +2,28 @@ class Solution {
     public boolean canJump(int[] nums) {
 
     
+// int n=nums.length-1;
 
+//         for(int i=n-1;i>=0;i--){
+//             if((nums[i]+i)>=n){
+//                 n=i;
+//             }
+//         }
 
-        int n=nums.length-1;
+//         return n==0;
 
-        for(int i=n-1;i>=0;i--){
-            if((nums[i]+i)>=n){
-                n=i;
-            }
+        int n=nums.length;
+        int maxindex=0;
+
+        for(int i=0;i<n;i++){
+
+            if(i>maxindex) return false;
+
+            maxindex=Math.max(maxindex,nums[i]+i);
+
         }
 
-        return n==0;
+        return true;
 
     }
 }
