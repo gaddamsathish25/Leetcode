6898550723fe00526a396d8d0c1a7b54b01284d2
Gaddam Sathish <gaddam.sathish2510@gmail.com>
2026-05-07 20:48:08 +0530

    Time: 14 ms (23.93%) | Memory: 52.5 MB (90.59%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,27 +1,62 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int count = 0;
-        int n = nums.length - 1;
-        int elment=0;
-        for (int i = 0; i <=n; i++) {
-            if (count == 0) {
-                elment = nums[i];
-                count = 1;
-            } else if (nums[i] == elment) {
-                count++;
-            } else if (nums[i] != elment) {
-                count--;
-            }
-        }
-        // int cnt = 0;
-        // for (int i = 0; i < n; i++) {
-        //     if (nums[i]==elment) {
-        //         cnt++;
+        // int count = 0;
+        // int n = nums.length - 1;
+        // int elment=0;
+        // for(int i=0;i<=n;i++){
+        //     if(count==0){
+        //         elment=nums[i];
+        //         count=1;
+        //     }else if(nums[i]==elment){
+        //         count++;
+        //     }else if(nums[i]!=elment){
+        //         count--;
         //     }
         // }
-        // if (cnt > (nums.length / 2)) {
-        //     return elment;
-        // }
-        return elment;
+        // return elment;
+
+
+
+
+        // // int cnt = 0;
+        // // for (int i = 0; i < n; i++) {
+        // //     if (nums[i]==elment) {
+        // //         cnt++;
+        // //     }
+        // // }
+        // // if (cnt > (nums.length / 2)) {
+        // //     return elment;
+        // // }
+
+
+
+
+        //using hashmap
+
+        HashMap<Integer,Integer> hm=new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            int val=nums[i];
+            if(hm.containsKey(val)){
+                int prev=hm.get(val);
+                hm.put(val,prev+1);
+            }else{
+                hm.put(val,1);
+            }
+        }
+
+        int element=0;
+
+        int max=0;
+
+        for(int x:hm.keySet()){
+            if(hm.get(x)>max){
+                max=hm.get(x);
+                element=x;
+            }
+        }
+
+        return element;
+        
     }
 }
